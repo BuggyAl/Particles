a0bc125c8d72059cca5b8b1e0aea044f93e18b69
Alex <buggyal.dev@gmail.com>
2025-06-20 16:04:55 -0500

    attempting migration to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
+}
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
+}
+
+group = "me.buggyal"
+version = "1.0.1"
+
+java {
+    withSourcesJar()
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
